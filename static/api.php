dc3bd6bcaee7a8012a488a7767e860c85cdd0667
Андрей Туманов <avtuman@gmail.com>
2018-09-24 14:29:45 +0300

    post api

--- static/api.php
+++ static/api.php
@@ -1,56 +1,75 @@
 <?php
-function set_cors() {
+function set_cors()
+{
     // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+    if (isset($_SERVER["HTTP_ORIGIN"])) {
+        // Decide if the origin in $_SERVER["HTTP_ORIGIN"] is one
         // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        header("Access-Control-Allow-Origin: {$_SERVER["HTTP_ORIGIN"]}");
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Max-Age: 86400"); // cache for 1 day
     }
 
     // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
 
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+        if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
+        // may also be using PUT, PATCH, HEAD etc
+        {
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        }
 
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) {
+            header("Access-Control-Allow-Headers: {$_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]}");
+        }
 
         exit(0);
     }
 }
 
 $authors = array(
-	array(
-		"id" => 1,
+    array(
+        "id" => 1,
         "books" => 1,
         "author" => "Акунин",
-		"isActive" => false,
-		"_rowVariant" => ""
-	),
-	array(
-		"id" => 2,
+        "isActive" => false,
+        "_rowVariant" => "",
+    ),
+    array(
+        "id" => 2,
         "books" => 5,
         "author" => "Бушков",
-		"isActive" => false,
-		"_rowVariant" => ""
-	),
-	array(
-		"id" => 3,
+        "isActive" => false,
+        "_rowVariant" => "",
+    ),
+    array(
+        "id" => 3,
         "books" => 3,
         "author" => "Пехов",
-		"isActive" => false,
-		"_rowVariant" => ""
-	)
+        "isActive" => false,
+        "_rowVariant" => "",
+    ),
 );
 
 set_cors();
 
-$res=array('data'=>array(),'success'=>true,'error'=>'');
-$res['data'] = $authors;
+$res = array("data" => array(), "success" => true, "error" => "");
 
+$_POST = json_decode(file_get_contents('php://input'), true);
 
-echo json_encode($res);
+if (isset($_POST["cmd"])) {
+    switch ($_POST['cmd']) {
+        case 'a_list':
+            $res['data'] = $authors;
+            echo json_encode($res);
+            break;
+        default:
+            $res['success'] = false;
+            $res['error'] = 'Unknown command';
+            echo json_encode($res);
+    }
+} else {
+    $res['success'] = false;
+    $res['error'] = 'No command';
+    echo json_encode($res);
+}
