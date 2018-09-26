6918b43ca43dc2e7745af78825f103d24e576f35
Андрей Туманов <avtuman@gmail.com>
2018-09-26 16:04:14 +0300

    Add files via upload

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="d-flex h-100 justify-content-center align-items-center">
-    <b-form @submit.prevent="login" class="login-form">
+    <b-form @submit.prevent="login" class="login-form" validated: true>
       <h2 class="text-center">Войти в систему</h2>
-      <h6 class="text-center">Пожалуйста введите своий логин и пароль</h6>
+      <h6 class="text-center">Пожалуйста введите свой логин и пароль</h6>
       <b-form-group id="loginInputGroup" label="Логин:" label-for="loginInput">
         <b-form-input id="loginInput" type="text" v-model="username" required placeholder="Введите имя пользователя">
         </b-form-input>
@@ -11,46 +11,52 @@
         <b-form-input id="passwordInput" type="password" v-model="password" required placeholder="Введите пароль">
         </b-form-input>
       </b-form-group>
-      <b-button type="submit" variant="primary" class="btn-block">Войти</b-button>
+      <b-form-row>
+        <b-col>
+          <b-button type="submit" variant="success" class="btn-block">Войти</b-button>
+        </b-col>
+      </b-form-row>
+      <b-form-row class="mt-2">
+        <b-col class="text-center">
+          <b-link to="registration">Зарегистрироваться</b-link>
+        </b-col>
+      </b-form-row>
     </b-form>
   </div>
 </template>
 
 <style lang="scss">
-  .login-form {
-    width: 25rem;
-    padding-bottom: 10rem;
-  }
+.login-form {
+  width: 25rem;
+  padding-bottom: 10rem;
+}
 </style>
 
 <script>
-  import {
-    AUTH_REQUEST
-  } from "../../store/actions/auth";
-  
-  export default {
-    name: "login",
-    data() {
-      return {
-        username: "",
-        password: ""
-      };
-    },
-    methods: {
-      login: function() {
-        const {
+import { AUTH_REQUEST } from "../../store/actions/auth";
+
+
+export default {
+  name: "login",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    login: function() {
+      const { username, password } = this;
+      
+      this.$store
+        .dispatch(AUTH_REQUEST, {
           username,
           password
-        } = this;
-        this.$store
-          .dispatch(AUTH_REQUEST, {
-            username,
-            password
-          })
-          .then(() => {
-            this.$router.push("/");
-          });
-      }
+        })
+        .then(() => {
+          this.$router.push("/");
+        });
     }
-  };
+  }
+};
 </script>
