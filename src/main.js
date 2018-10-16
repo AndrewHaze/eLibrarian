2daea9265e70be9702d7d242f539062b4188d6ab
Андрей Туманов <avtuman@gmail.com>
2018-10-16 16:50:46 +0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from "axios"
 
 import Loading from './components/lib/loading'
 
@@ -12,6 +13,33 @@ Vue.use(BootstrapVue)
 Vue.component('loading', Loading)
 
 Vue.config.productionTip = false
+
+Vue.mixin({
+  methods: {
+    callApi(url, prms, callback) {
+      axios({
+          method: "post",
+          url: url,
+          data: prms
+        })
+        .then(response => {
+          // в response.data получаем JSON,
+          // сервер формирует обязательные поля data,success,error
+          let rspData = response.data;
+          if (!rspData.success) {
+            this.setServerError(rspData.error, "");
+          } else {
+            callback(rspData.data);
+          }
+        })
+        .catch(error => {
+          this.setServerError(error.message, error.stack);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+    }
+  }
+})
 
 new Vue({
   el: '#app',
