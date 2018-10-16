25eef40ced84d4b1a18bc33a136c3197ea829825
Андрей Туманов <avtuman@gmail.com>
2018-10-16 16:51:18 +0300

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
 import auth from './modules/auth'
+import php_api from './modules/php_api'
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
@@ -10,6 +11,8 @@ export default new Vuex.Store({
   modules: {
     user,
     auth,
+    php_api,
   },
   strict: debug,
+  
 })
