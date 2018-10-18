53b91523ee574c5bbb3ac27a7c70766ee554b4b1
Андрей Туманов <avtuman@gmail.com>
2018-10-18 17:11:48 +0300

    Add files via upload

--- static/api.php
+++ static/api.php
@@ -1,48 +1,21 @@
 <?php
-function set_cors()
-{
-    // Allow from any origin
-    if (isset($_SERVER["HTTP_ORIGIN"])) {
-        // Decide if the origin in $_SERVER["HTTP_ORIGIN"] is one
-        // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: {$_SERVER["HTTP_ORIGIN"]}");
-        header("Access-Control-Allow-Credentials: true");
-        header("Access-Control-Max-Age: 86400"); // cache for 1 day
-    }
-
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
-
-        if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
-        // may also be using PUT, PATCH, HEAD etc
-        {
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-        }
-
-        if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) {
-            header("Access-Control-Allow-Headers: {$_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]}");
-        }
-
-        exit(0);
-    }
-}
 
 $chars = array(
     array(
-        "text" => '*',
-        "value" => '*',
+        "text" => "*",
+        "value" => "*",
     ),
     array(
-        "text" => 'А',
-        "value" => 'А',
+        "text" => "А",
+        "value" => "А",
     ),
     array(
-        "text" => 'Б',
-        "value" => 'Б',
+        "text" => "Б",
+        "value" => "Б",
     ),
     array(
-        "text" => 'П',
-        "value" => 'П',
+        "text" => "П",
+        "value" => "П",
     ),
 );
 
@@ -77,49 +50,100 @@ $authors = array(
     ),
 );
 
+function set_cors()
+{
+    // Allow from any origin
+    if (isset($_SERVER["HTTP_ORIGIN"])) {
+        // Decide if the origin in $_SERVER["HTTP_ORIGIN"] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER["HTTP_ORIGIN"]}");
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Max-Age: 86400"); // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+
+        if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
+        // may also be using PUT, PATCH, HEAD etc
+        {
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        }
+
+        if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) {
+            header("Access-Control-Allow-Headers: {$_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]}");
+        }
+
+        exit(0);
+    }
+}
+
+function pdo_connect()
+{
+    $host = "127.0.0.1";
+    $db = "elib";
+    $user = "root";
+    $pass = "";
+    $charset = "utf8";
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
+    return $pdo = new PDO($dsn, $user, $pass, $opt);
+}
+
 set_cors();
 
 $res = array("data" => array(), "success" => true, "error" => "");
 
-
-//$pdo = null;
-
-$_POST = json_decode(file_get_contents('php://input'), true);
+$_POST = json_decode(file_get_contents("php://input"), true);
 
 if (isset($_POST["cmd"])) {
-    switch ($_POST['cmd']) {
-        case 'connect':
-            $host = '127.0.0.1';
-            $db = 'elib';
-            $user = 'root';
-            $pass = '';
-            $charset = 'utf8';
-
-            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-            $opt = [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false,
-            ];
-            $pdo = new PDO($dsn, $user, $pass, $opt);
+    switch ($_POST["cmd"]) {
+        case "first":
+            $pdo = pdo_connect();
             if ($pdo) {
-                $res['data'] = "true";
-                $connect = true;
+                $stmt = $pdo->query("SELECT COUNT(*) FROM users;");
+                $count = $stmt->fetchColumn();
+            }
+            if ($count > 0) {
+                $res["data"] = false;
+            } else {
+                $res["data"] = true;
             }
             break;
-        case 'с_list':
-            $res['data'] = $chars;
+        case "exist":
+            $pdo = pdo_connect();
+            if ($pdo) {
+                $username = $_POST["username"];
+                $stmt = $dbh->prepare('SELECT COUNT(*) FROM users WHERE users=:login');
+                $stmt->bindValue(':login', $username, PDO::PARAM_INT);
+                $stmt->execute();
+                $count = $stmt->fetchColumn();
+                if ($count > 0) {
+                    $res["data"] = false;
+                } else {
+                    $res["data"] = true;
+                }
+            }
             break;
-        case 'a_list':
-            $res['data'] = $authors;
+        case "с_list":
+            $res["data"] = $chars;
+            break;
+        case "a_list":
+            $res["data"] = $authors;
             break;
         default:
-            $res['success'] = false;
-            $res['error'] = 'Unknown command';
+            $res["success"] = false;
+            $res["error"] = "Unknown command";
     }
 } else {
-    $res['success'] = false;
-    $res['error'] = 'No command';
+    $res["success"] = false;
+    $res["error"] = "No command";
 }
 
 echo json_encode($res);
