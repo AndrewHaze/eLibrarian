db5d77363ac2725b35ddbdb44fd74940b9003f00
Андрей Туманов <avtuman@gmail.com>
2018-10-18 17:12:26 +0300

    Add files via upload

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -4,12 +4,20 @@
       <h2 class="text-center">Войти в систему</h2>
       <h6 class="text-center">Пожалуйста введите свой логин и пароль</h6>
       <b-form-group id="loginInputGroup" label="Логин:" label-for="loginInput">
-        <b-form-input id="loginInput" type="text" v-model="username" required placeholder="Введите имя пользователя">
+        <b-form-input id="loginInput" type="text" :state="loginState" v-model="username" placeholder="Введите имя пользователя">
         </b-form-input>
+        <b-form-invalid-feedback id="inputLiveFeedback1">
+          <!-- This will only be shown if the preceeding input has an invalid state -->
+          {{ usernameErrorMessage }}
+        </b-form-invalid-feedback>
       </b-form-group>
       <b-form-group id="passwordInputGroup" label="Пароль:" label-for="passwordInput">
-        <b-form-input id="passwordInput" type="password" v-model="password" required placeholder="Введите пароль">
+        <b-form-input id="passwordInput" type="password" :state="passwordState" v-model="password" placeholder="Введите пароль">
         </b-form-input>
+        <b-form-invalid-feedback id="inputLiveFeedback2">
+          <!-- This will only be shown if the preceeding input has an invalid state -->
+          {{ passwordErrorMessage }}
+        </b-form-invalid-feedback>
       </b-form-group>
       <b-form-row>
         <b-col>
@@ -26,58 +34,82 @@
 </template>
 
 <style lang="scss">
-.login-form {
-  width: 25rem;
-  padding-bottom: 10rem;
-}
+  .login-form {
+    width: 25rem;
+    padding-bottom: 10rem;
+  }
 </style>
 
 <script>
-import store from "../../store";
-import { AUTH_REQUEST } from "../../store/actions/auth";
-
-
-export default {
-  name: "login",
-  data() {
-    return {
-      username: "",
-      password: "",
-      
-    };
-  },
-  created: function() {
-    const self = this;
-    //Вызов функции из глобального миксина
-    this.callApi(
-      this.$store.getters.prefix + "/static/api.php",
-      {
-        cmd: "connect",
-        dat: ""
-      },
-      function(rd) {
-        store.state.connect = rd;
-        
-      }
-    );
-    console.log(store.state.connect);
-    if (store.state.connect) {
-        console.log('connect');
-    }
-  },
-  methods: {
-    login: function() {
-      const { username, password } = this;
-
-      this.$store
-        .dispatch(AUTH_REQUEST, {
+  import store from "../../store";
+  import {
+    AUTH_REQUEST
+  } from "../../store/actions/auth";
+  
+  export default {
+    name: "login",
+    data() {
+      return {
+        username: "",
+        usernameErrorMessage: "Пожалуйста заполните это поле",
+        loginState: null,
+        password: "",
+        passwordErrorMessage: "Пожалуйста заполните это поле",
+        passwordState: null,
+      };
+    },
+    created: function() {
+      const self = this;
+      //Вызов функции из глобального миксина
+      this.callApi(
+        this.$store.getters.prefix + "/static/api.php", {
+          cmd: "first", //проверяем наличие зарег. пользователей
+          dat: ""
+        },
+        function(rd) {
+          if (rd) {
+            //нет зарег. пользователей
+            store.commit("checkUsers", false);
+            self.$router.push("/registration");
+          } else {
+            store.commit("checkUsers", true);
+          }
+        }
+      );
+    },
+    methods: {
+      login: function() {
+        const {
           username,
           password
-        })
-        .then(() => {
-          this.$router.push("/");
-        });
+        } = this;
+  
+        let problems = false;
+        if (username.length < 1) {
+          problems = true;
+          this.loginState = false;
+        } else {
+          problems = false;
+          this.loginState = true;
+        }
+        if (password.length < 1) {
+          problems = true;
+          this.passwordState = false;
+        } else {
+          problems = false;
+          this.passwordState = true;
+        }
+        if (problems) return;
+  
+        this.$store
+          .dispatch(AUTH_REQUEST, {
+            username,
+            password
+          })
+          .then(() => {
+            this.$router.push("/");
+          });
+      }
     }
-  }
-};
+  };
 </script>
