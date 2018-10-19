1a1e4ed4ed3b4d064245265572a32070a782d2aa
Андрей Туманов <avtuman@gmail.com>
2018-10-19 16:03:13 +0300

    комментарии

--- src/store/modules/php_api.js
+++ src/store/modules/php_api.js
@@ -1,7 +1,9 @@
 const state = { link: "http://l.mgr.loc", possibility: true }
 
 const getters = {
+  //домен для подстановки в пути к файлам
   prefix: state => state.link,
+  //при отсутвии юзеров с формы входа перекидываем на регистрацию
   login_form: state => state.possibility,
 }
 
