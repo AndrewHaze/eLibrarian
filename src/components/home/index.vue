b56ca108748ebcfb44adb55d0aca86c01147d452
Андрей Туманов <avtuman@gmail.com>
2018-10-23 17:54:27 +0300

    Add files via upload

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -2,6 +2,27 @@
   <section>
     <loading v-if="loading" />
     <div v-if="isAuthenticated">
+      <b-modal v-model="congShow" no-close-on-backdrop ok-only :title="appTitle">
+        <div class="d-block text-center">
+          <h3>Поздравляем! Вы успешно зарегистрировались!</h3>
+        </div>
+      </b-modal>
+      <b-modal id="bookScanner" no-close-on-backdrop hide-footer size="lg" title="Импорт книг">
+        <b-container fluid>
+          <b-row class="mb-1">
+            <b-col>
+              <b-button-toolbar key-nav aria-label="Toolbar with button groups">
+                <b-button-group class="e-btn-group mr-2">
+                  <b-button variant="primary">Обработать папку</b-button>
+                  <b-button variant="success">Добавить папку</b-button>
+                  <b-button variant="success">Добавить файлы</b-button>
+                </b-button-group>
+                <b-button variant="danger">Запуск</b-button>
+              </b-button-toolbar>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-modal>
       <b-row class="z100">
         <b-col>
           <b-tabs>
@@ -47,12 +68,20 @@
     cursor: pointer;
     user-select: none;
   }
+  
+  .e-btn-group {
+    &>.btn+.btn {
+      margin-left: 1px;
+    }
+    
+  }
 </style>
 
 <script>
   import {
     mapGetters
   } from 'vuex';
+  import store from "../../store";
   import Login from '../login';
   import AuthorsTab from "../tab-authors";
   import GenresTab from "../tab-genres";
@@ -67,13 +96,29 @@
       SeriesTab
     },
     computed: {
-      ...mapGetters(['isAuthenticated', 'authStatus']),
+      ...mapGetters(['isAuthenticated', 'authStatus', 'congratulation', 'appTitle']),
       loading: function() {
         return this.authStatus === 'loading' && !this.isAuthenticated
+      },
+  
+    },
+    created: function() {
+      if (this.congratulation) {
+        this.congShow = true;
       }
     },
+    methods: {
+      // openFiles: function() {
+      //   let file_input = document.createElement("input");
+      //   file_input.type = "file";
+      //   file_input.click();
+      // },
+    },
     data() {
-      return ({})
+      return ({
+        congShow: false,
+      })
     },
   }
 </script>
+
