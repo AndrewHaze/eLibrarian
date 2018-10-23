8bc69ee786855ae834873704806f268fa4a1705a
Андрей Туманов <avtuman@gmail.com>
2018-10-23 17:54:48 +0300

    Add files via upload

--- src/store/modules/php_api.js
+++ src/store/modules/php_api.js
@@ -1,15 +1,21 @@
-const state = { link: "http://l.mgr.loc", possibility: true }
+const state = { link: "http://l.mgr.loc", possibility: true, reg: false, at: 'eLibrarian v.0.0.1' }
 
 const getters = {
   //домен для подстановки в пути к файлам
   prefix: state => state.link,
-  //при отсутвии юзеров с формы входа перекидываем на регистрацию
-  login_form: state => state.possibility,
+  //флаг: при отсутвии юзеров на экране рег. не показываем ссылку на вход
+  loginForm: state => state.possibility,
+  //флаг: окно после регистрации 
+  congratulation: state => state.reg,
+  appTitle: state => state.at,
 }
 
 const mutations = {
   checkUsers(state, available) {
     state.possibility = available
+  },
+  showRegModal(state, show) {
+    state.reg = show
   }
 }
 
