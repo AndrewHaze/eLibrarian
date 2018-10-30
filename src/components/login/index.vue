0f07deb477e89e67cca1554be9b02ef701bf52e8
Андрей Туманов <avtuman@gmail.com>
2018-10-30 16:08:11 +0300

    Add files via upload

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -34,126 +34,121 @@
 </template>
 
 <style lang="scss">
-  .login-form {
-    width: 25rem;
-    padding-bottom: 10rem;
-  }
+.login-form {
+  width: 25rem;
+  padding-bottom: 10rem;
+}
 </style>
 
 <script>
-  import store from "../../store";
-  import {
-    AUTH_REQUEST
-  } from "../../store/actions/auth";
-  
-  export default {
-    name: "login",
-    data() {
-      return {
-        username: "",
-        usernameErrorMessage: "Пожалуйста заполните это поле",
-        loginState: null,
-        password: "",
-        passwordErrorMessage: "Пожалуйста заполните это поле",
-        passwordState: null,
-      };
-    },
-    created: function() {
+import store from "../../store";
+import { AUTH_REQUEST } from "../../store/actions/auth";
+
+export default {
+  name: "login",
+  data() {
+    return {
+      username: "",
+      usernameErrorMessage: "Пожалуйста заполните это поле",
+      loginState: null,
+      password: "",
+      passwordErrorMessage: "Пожалуйста заполните это поле",
+      passwordState: null
+    };
+  },
+  created: function() {
+    const self = this;
+    //Вызов функции из глобального миксина
+    this.callApi(
+      this.$store.getters.prefix + "/static/api.php",
+      {
+        cmd: "first", //проверяем наличие зарег. пользователей
+        dat: ""
+      },
+      "",
+      function(rd) {
+        if (rd) {
+          //нет зарег. пользователей
+          store.commit("checkUsers", false);
+          self.$router.push("/registration");
+        } else {
+          store.commit("checkUsers", true);
+        }
+      }
+    );
+  },
+  methods: {
+    verification: function() {
+      const { username, password } = this;
       const self = this;
-      //Вызов функции из глобального миксина
       this.callApi(
-        this.$store.getters.prefix + "/static/api.php", {
-          cmd: "first", //проверяем наличие зарег. пользователей
-          dat: ""
+        this.$store.getters.prefix + "/static/api.php",
+        {
+          cmd: "verification", //проверяем наличие зарег. пользователей
+          usr: username,
+          psw: password
         },
+        "",
         function(rd) {
           if (rd) {
-            //нет зарег. пользователей
-            store.commit("checkUsers", false);
-            self.$router.push("/registration");
+            store.commit("showRegModal", false);
+            self.$store
+              .dispatch(AUTH_REQUEST, {
+                username,
+                password
+              })
+              .then(() => {
+                self.$router.push("/");
+              });
           } else {
-            store.commit("checkUsers", true);
+            self.loginState = true;
+            self.passwordState = false;
+            self.passwordErrorMessage = "Неправильный пароль!";
           }
         }
       );
     },
-    methods: {
-      verification: function() {
-        const {
-          username,
-          password
-        } = this;
-        const self = this;
-        this.callApi(
-          this.$store.getters.prefix + "/static/api.php", {
-            cmd: "verification", //проверяем наличие зарег. пользователей
-            usr: username,
-            psw: password
-          },
-          function(rd) {
-            if (rd) {
-              store.commit("showRegModal", false);
-              self.$store
-                .dispatch(AUTH_REQUEST, {
-                  username,
-                  password
-                })
-                .then(() => {
-                  self.$router.push("/");
-                });
-            } else {
-              self.loginState = true;
-              self.passwordState = false;
-              self.passwordErrorMessage =
-                "Неправильный пароль!";
-            }
-          }
-        );
-  
-      },
-      login: function() {
-        const self = this;
-        const {
-          username,
-          password
-        } = this;
-        let flag;
-        let problems = false;
-        if (username.length < 1) {
-          problems = true;
-          this.loginState = false;
-        } else {
-          problems = false;
-          this.loginState = true;
-        }
-        if (password.length < 1) {
-          problems = true;
-          this.passwordState = false;
-        } else {
-          problems = false;
-          this.passwordState = true;
-        }
-        if (problems) return;
-  
-        //Вызов функции из глобального миксина
-        this.callApi(
-          this.$store.getters.prefix + "/static/api.php", {
-            cmd: "exist", //проверяем наличие зарег. пользователей
-            dat: username
-          },
-          function(rd) {
-            if (rd) {
-              self.loginState = false;
-              self.passwordState = true;
-              self.usernameErrorMessage =
-                "Пользователь с таким именем не существует!";
-            } else {
-              self.verification();
-  
-            }
-          }
-        );
+    login: function() {
+      const self = this;
+      const { username, password } = this;
+      let flag;
+      let problems = false;
+      if (username.length < 1) {
+        problems = true;
+        this.loginState = false;
+      } else {
+        problems = false;
+        this.loginState = true;
       }
+      if (password.length < 1) {
+        problems = true;
+        this.passwordState = false;
+      } else {
+        problems = false;
+        this.passwordState = true;
+      }
+      if (problems) return;
+
+      //Вызов функции из глобального миксина
+      this.callApi(
+        this.$store.getters.prefix + "/static/api.php",
+        {
+          cmd: "exist", //проверяем наличие зарег. пользователей
+          dat: username
+        },
+        "",
+        function(rd) {
+          if (rd) {
+            self.loginState = false;
+            self.passwordState = true;
+            self.usernameErrorMessage =
+              "Пользователь с таким именем не существует!";
+          } else {
+            self.verification();
+          }
+        }
+      );
     }
-  };
+  }
+};
 </script>
