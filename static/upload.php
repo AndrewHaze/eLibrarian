f585ffaaafbd4488d04ce0890389ad79e1400cca
Андрей Туманов <avtuman@gmail.com>
2018-11-09 17:02:23 +0300

    Add files via upload

--- static/upload.php
+++ static/upload.php
@@ -16,12 +16,14 @@ if (!file_exists($target_dir)) {
 
 $name = basename($_FILES["file"]["name"]);
 
-$target_file = $target_dir . uniqid($_SESSION["user"] . "_", true) . "." . pathinfo($name, PATHINFO_EXTENSION);
+$new_name = uniqid($_SESSION["user"] . "_", true) . "." . pathinfo($name, PATHINFO_EXTENSION);
+
+$target_file = $target_dir . $new_name;
 
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file) && ($_SESSION["user"] != null)) {
     $res["data"] = array(
         "base_name" => $name,
-        "hash_name" => $target_file,
+        "hash_name" => $new_name,
         "status" => "raw",
     );
 } else {
