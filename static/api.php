d7fa82bb96b6bf69fb3f023c745fb1632f98a55a
Андрей Туманов <avtuman@gmail.com>
2018-11-15 16:58:17 +0300

    db

--- static/api.php
+++ static/api.php
@@ -1,7 +1,7 @@
 <?php
 //Включение буферизации вывода
 //что-бы не портить JSON
-ob_start(); 
+ob_start();
 
 $chars = array(
     array(
@@ -27,28 +27,28 @@ $authors = array(
         "id" => 1,
         "books" => 1,
         "author" => "Акунин",
-        "isActive" => FALSE,
+        "isActive" => false,
         "_rowVariant" => "",
     ),
     array(
         "id" => 2,
         "books" => 15,
         "author" => "Бушков",
-        "isActive" => FALSE,
+        "isActive" => false,
         "_rowVariant" => "",
     ),
     array(
         "id" => 3,
         "books" => 3,
         "author" => "Пехов",
-        "isActive" => FALSE,
+        "isActive" => false,
         "_rowVariant" => "",
     ),
     array(
         "id" => 4,
         "books" => 12,
         "author" => "Панов",
-        "isActive" => FALSE,
+        "isActive" => false,
         "_rowVariant" => "",
     ),
 );
@@ -57,9 +57,9 @@ require_once 'functions.php';
 
 set_cors();
 
-$res = array("data" => array(), "success" => TRUE, "error" => "");
+$res = array("data" => array(), "success" => true, "error" => "");
 
-$_POST = json_decode(file_get_contents("php://input"), TRUE);
+$_POST = json_decode(file_get_contents("php://input"), true);
 
 if (isset($_POST["cmd"])) {
     session_name("xeg6joZqNSGP3FyEi6xW");
@@ -71,12 +71,12 @@ if (isset($_POST["cmd"])) {
                 $stmt = $pdo->query("SELECT COUNT(*) FROM users;");
                 $count = $stmt->fetchColumn();
                 if ($count > 0) {
-                    $res["data"] = FALSE;
+                    $res["data"] = false;
                 } else {
-                    $res["data"] = TRUE;
+                    $res["data"] = true;
                 }
             } else {
-                $res["success"] = FALSE;
+                $res["success"] = false;
                 $res["error"] = "PDO Error";
             }
             break;
@@ -88,12 +88,12 @@ if (isset($_POST["cmd"])) {
                 $stmt->execute();
                 $count = $stmt->fetchColumn();
                 if ($count > 0) {
-                    $res["data"] = FALSE;
+                    $res["data"] = false;
                 } else {
-                    $res["data"] = TRUE;
+                    $res["data"] = true;
                 }
             } else {
-                $res["success"] = FALSE;
+                $res["success"] = false;
                 $res["error"] = "PDO Error";
             }
             break;
@@ -102,14 +102,14 @@ if (isset($_POST["cmd"])) {
                 $username = $_POST["usr"];
                 $password = $_POST["psw"];
                 $hash = password_hash($password, PASSWORD_DEFAULT);
-                $stmt = $pdo->prepare('INSERT INTO `users` (`ur_id`, `ur_login`, `ur_hash`) VALUES (NULL, :login, :hash);');
+                $stmt = $pdo->prepare('INSERT INTO `users` ( `ur_login`, `ur_hash`) VALUES (:login, :hash);');
                 $stmt->bindValue(':login', $username, PDO::PARAM_INT);
                 $stmt->bindValue(':hash', $hash, PDO::PARAM_INT);
                 $stmt->execute();
                 $_SESSION['user'] = $username;
                 $res["data"] = $_SESSION['user'];
             } else {
-                $_SESSION['user'] = NULL;
+                $_SESSION['user'] = null;
                 $res["error"] = "PDO Error";
             }
             break;
@@ -125,13 +125,13 @@ if (isset($_POST["cmd"])) {
                     $_SESSION['user'] = $username;
                     $res["data"] = $_SESSION['user'];
                 } else {
-                    $_SESSION['user'] = NULL;
-                    $res["data"] = FALSE;
+                    $_SESSION['user'] = null;
+                    $res["data"] = false;
                     $res["error"] = "Wrong Password";
                 }
             } else {
-                $_SESSION['user'] = NULL;
-                $res["success"] = FALSE;
+                $_SESSION['user'] = null;
+                $res["success"] = false;
                 $res["error"] = "PDO Error";
             }
             break;
@@ -147,31 +147,33 @@ if (isset($_POST["cmd"])) {
         case "clear_upload": //очистка папки uploads, для текщей сессии
             $res["data"] = clear_dir('uploads');
             if (!$res["data"]) {
-                $res["success"] = FALSE;
+                $res["success"] = false;
                 $res["error"] = "I/O Error (Clear Upload)";
             }
             break;
         case "proc":
             if ($pdo) {
                 if ($_SESSION["user"]) {
-                    $filename = 'uploads/' . $_POST["file"];
-                    $fileext = pathinfo($filename, PATHINFO_EXTENSION);
+                    $username = $_SESSION["user"];
+                    $filename = $_POST["file"];
+                    $file = 'uploads/' . $filename;
+                    $fileext = pathinfo($file, PATHINFO_EXTENSION);
                     //Создаем XML документ
                     $doc = new DOMDocument();
                     //Отключаем проверку ошибок
-                    $doc->strictErrorChecking = FALSE;
-                    $doc->recover = TRUE;
+                    $doc->strictErrorChecking = false;
+                    $doc->recover = true;
                     if ($fileext == "fb2") {
                         //Загружаем содержимое файла
-                        $load = $doc->load($filename, LIBXML_NOERROR);
+                        $load = $doc->load($file, LIBXML_NOERROR);
                     } else if ($fileext == "zip") {
                         $zip = new ZipArchive();
-                        if ($zip->open($filename)) {
+                        if ($zip->open($file)) {
                             $res["error"] = "per";
                             $data = $zip->getFromIndex(0);
                             $load = $doc->loadXML($data);
                         } else {
-                            $load = FALSE;
+                            $load = false;
                             $res["error"] = "dma";
                         }
                     }
@@ -209,9 +211,9 @@ if (isset($_POST["cmd"])) {
                             //Название книги
                             $book_title = $title_info->getElementsByTagName('book-title')->item(0)->nodeValue;
                             //Аннотация
-                            $annotation = trim($title_info->getElementsByTagName('annotation')->item(0)->nodeValue);
+                            $book_annotation = trim($title_info->getElementsByTagName('annotation')->item(0)->nodeValue);
                             //Дата
-                            $date = $title_info->getElementsByTagName('date')->item(0)->nodeValue;
+                            $book_date = $title_info->getElementsByTagName('date')->item(0)->nodeValue;
                             //Серия
                             if (empty($title_info->getElementsByTagName('sequence')->item(0))) {
                                 $sequence = '';
@@ -220,17 +222,31 @@ if (isset($_POST["cmd"])) {
                                 $sequence = $title_info->getElementsByTagName('sequence')->item(0)->GetAttribute('name');
                                 $sequence_number = $title_info->getElementsByTagName('sequence')->item(0)->GetAttribute('number');
                             }
+                            //books.db
+                            $time = strtotime($book_date);
+                            echo $time;
+                            $stmt = $pdo->prepare('INSERT INTO `books` (`bk_owner`, `bk_title`, `bk_annotation`, `bk_file_date`, `bk_file`)
+                                                   VALUES ((SELECT ur_id FROM users WHERE ur_login = :login), :book_title, :book_annotation, :book_date, :book_file);');
+                            $stmt->bindValue(':login', $username, PDO::PARAM_INT);
+                            $stmt->bindValue(':book_title', $book_title, PDO::PARAM_INT);
+                            $stmt->bindValue(':book_annotation', $book_annotation, PDO::PARAM_INT);
+                            $stmt->bindValue(':book_date', date('Y-m-d', $time), PDO::PARAM_INT);
+                            $stmt->bindValue(':book_file', $filename, PDO::PARAM_INT);
+                            if ($stmt->execute()) {
+                                $res["data"] = array(
+                                    "hash_name" => $filename,
+                                );
+                            } else {
+                                $res["success"] = false;
+                                $res["error"] = "dbe";
+                            }
 
-                            $res["data"] = array(
-                                "book_title" => $book_title,
-                                "hash_name" => $filename,
-                            );
                         } else {
-                            $res["success"] = FALSE;
+                            $res["success"] = false;
                             $res["error"] = "ndf";
                         }
                     } else {
-                        $res["success"] = FALSE;
+                        $res["success"] = false;
                     }
                 }
             } else {
@@ -238,11 +254,11 @@ if (isset($_POST["cmd"])) {
             }
             break;
         default:
-            $res["success"] = FALSE;
+            $res["success"] = false;
             $res["error"] = "Unknown command";
     }
 } else {
-    $res["success"] = FALSE;
+    $res["success"] = false;
     $res["error"] = "No command";
 }
 ob_end_clean(); //Чистим буфер
