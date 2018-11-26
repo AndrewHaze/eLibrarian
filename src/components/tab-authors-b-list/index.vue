9bdf38196085e71379e5e517cbc8e901b050a113
Андрей Туманов <avtuman@gmail.com>
2018-11-26 17:37:04 +0300

    db

--- src/components/tab-authors-b-list/index.vue
+++ src/components/tab-authors-b-list/index.vue
@@ -2,37 +2,88 @@
 <section>
     <h6 v-if="this.curAI == -1">Нет данных для отображения</h6>
     <div v-else>
-        <div v-for="item in bListItems" :key="item.id">
-            {{ item.author }}
-            {{ item.title }}
-        </div>
+      <div class="cover-book-list" >
+          <div class="item" v-for="item in bListItems" :key="item.id">
+              <div class = "cover">
+                <img :src="'data:image/jpg;base64,'+item.cover">
+              </div>
+              <div class = "info">
+                <div class="book-authors">{{ item.author }} </div>
+                <div class="book-title">{{ item.title }}</div>
+              </div>
+          </div>
+      </div>
     </div>
 </section>
 </template>
 
+<style lang="scss">
+.content  section {
+  padding-left: .5rem;
+}
+.cover-book-list {
+  display: flex;
+  flex-flow: row wrap;
+  div {
+    display: flex;
+  }
+  .item {
+    flex-flow: row nowrap;
+    width: 21rem;
+    .cover {
+      width: 150px;
+      min-width: 150px;
+      height: 220px;
+      margin-right: 1rem;
+      justify-content: center;
+      overflow: hidden;
+      > img {
+        height: 100%;
+      }
+    }
+    .info {
+      flex-flow: column nowrap;
+      line-height: 1.3;
+      .book-authors {
+        color: #4c4c4c;
+      }
+      .book-title {
+        font-size: 1.2rem;
+        font-weight: 550;
+        margin-top: .3rem;
+      }
+    }
+  }
+  .item + .item {
+    margin-right: 1rem;
+  }
+}
+</style>
+    
 <script>
 export default {
-    name: 'books-list',
-    props: ["curAI"],
-    data: function () {
-        return {
-            bListItems: [],
-        };
-    },
-    watch: {
-        curAI: function (val) {
-            const self = this;
-            this.callApi(
-                this.$store.getters.prefix + "/static/api.php", {
-                    cmd: "b_list",
-                    dat: val
-                },
-                "",
-                function (rd) {
-                    self.bListItems = rd; //возвр. данные (Responce)
-                }
-            );
+  name: "books-list",
+  props: ["curAI"],
+  data: function() {
+    return {
+      bListItems: []
+    };
+  },
+  watch: {
+    curAI: function(val) {
+      const self = this;
+      this.callApi(
+        this.$store.getters.prefix + "/static/api.php",
+        {
+          cmd: "b_list",
+          dat: val
+        },
+        "",
+        function(rd) {
+          self.bListItems = rd; //возвр. данные (Responce)
         }
+      );
     }
+  }
 };
 </script>
