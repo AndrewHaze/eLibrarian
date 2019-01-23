12c595c60e214883b7eeb3a38f18588b1bf43971
Андрей Туманов <avtuman@gmail.com>
2019-01-23 17:45:18 +0300

    Add files via upload

---
+++ src/components/reader/index.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  reader
+</div>  
+</template>
+
+<style lang="scss">
+$line-color: #dee2e6;
+$selected-color: #ddd;
+$hover-color: rgba(221, 221, 221, 0.4);
+
+%flex {
+  display: flex;
+  flex: row, nowrap;
+  justify-content: space-between;
+}
+
+
+</style>
+
+<script>
+import store from "../../store";
+
+export default {
+  name: "reader",
+  
+  data: function() {
+    return {
+      
+    };
+  },
+  beforeCreate: function() {
+    store.commit("setReader", true);
+  },
+};
+</script>
